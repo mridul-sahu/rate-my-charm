93600efeb69e77bc20c2e14faf086df4f8fe45f8
Mridul <mridulsahu01@gmail.com>
2016-12-16 14:37:39 +0530

    Lost

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
 
   get 'static_pages/home'
 
   get 'static_pages/ratings'
+
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
