0f0315c517a27f8acea70f6fa33ee83b88ebba1d
Mridul <mridulsahu01@gmail.com>
2016-12-19 13:27:26 +0530

    Security check

--- app/controllers/voting_controller.rb
+++ app/controllers/voting_controller.rb
@@ -1,10 +1,11 @@
 class VotingController < ApplicationController
-  before_action :persist_vote_session
+  before_action :persist_vote_session , only: [:new]
   def new
 
   end
 
   def vote_for
+    if session[:id_check].to_i == (params[:id_voted].to_i*params[:id_not_voted].to_i)
     @user1  = User.find(params[:id_voted])
     @user2  = User.find(params[:id_not_voted])
 
@@ -20,7 +21,12 @@ class VotingController < ApplicationController
 
     @user1.update_attributes :rating => @user1.rating
     @user2.update_attributes :rating => @user2.rating
+
     redirect_to voting_new_url
+    else
+      flash[:danger] = "You are Blocked"
+      redirect_to voting_new_url
+    end
 
   end
 
@@ -30,7 +36,7 @@ class VotingController < ApplicationController
       session[:gender] = params[:gender]
     end
     @users = User.where(gender: session[:gender]).order("RANDOM()")
-
     session[:gender] ||= "Male" #shoudn't happen
+    session[:id_check] = 0
   end
 end
