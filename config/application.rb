811ef831b2f2a38fbdcbee642fd8bb9b629bd191
Mridul <mridulsahu01@gmail.com>
2016-12-20 17:33:40 +0530

    Block 3

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.middleware.use Rack::Attack
+
 
 module Stud
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.use Rack::Attack
   end
 end
